c844fd94ea5b8d0f0928f6de5c96c1fe4537b82b
Navic V. <navic.vo@gmail.com>
2025-01-03 20:22:06 +0700

    Create a function to make square divs

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const container = document.querySelector('.container');
 
-container.innerHTML = `<h1>Hello World</h1>`;
+// Creating 16x16 grid of square divs
+let gridDimension = 16;
+let numberOfSquareDivs = gridDimension * gridDimension;
+for (let i = 1; i <= numberOfSquareDivs; i++ ) {
+    container.innerHTML += `<div class="square"></div>`;
+}
+
